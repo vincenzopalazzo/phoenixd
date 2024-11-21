ae8a97fca9dd979077dd2e499fdcb940b102ee74
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-11-21 12:33:56 +0100

    fixup! api: allow to create an offer with description
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 
 allprojects {
     group = "fr.acinq.lightning"
-    version = "0.4.3-SNAPSHOT"
+    version = "1.8.6-SNAPSHOT"
 
     repositories {
         // using the local maven repository with Kotlin Multi Platform can lead to build errors that are hard to diagnose.
